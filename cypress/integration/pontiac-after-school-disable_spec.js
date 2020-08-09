3de8b4cb76c508394d4db256ed5c93a46ef5bc29
Robert, Drew <ddrobe2@ilstu.edu>
2020-08-09 17:58:22 -0500

    automated solutions sometimes work

---
+++ cypress/integration/pontiac-after-school-disable_spec.js
@@ -0,0 +1,18 @@
+// disable setting that causes error on squarespace site to fail the test.
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+});
+
+it("disables pontiac after-school product", () => {
+    cy.visit("https://drew-robert-ekn6.squarespace.com/config/commerce/inventory");
+    cy.get(".username").type(Cypress.env("SQUARESPACE_USERNAME"));
+    cy.get(".password").type(Cypress.env("SQUARESPACE_PASSWORD"));
+    cy.get("[data-test=login-button] > span").click({force: true});
+    cy.get(".sqs-damask-panel-content").contains("Pontiac Weekly Fee");
+    cy.get("body").type("{esc}");
+    cy.wait(1000);
+    cy.get(".sqs-damask-panel-content").contains("Pontiac Weekly Fee").parents("[data-test=inventory-panel-row]").click({force: true});
+    cy.get(".standard-dialog-wrapper").contains("Visible").click({force: true});
+    cy.get(":nth-child(3) > .field-workflow-flyout-option > .field-workflow-title").click({force: true});
+    cy.get("[data-test=dialog-saveAndClose]").click({force: true});
+});
